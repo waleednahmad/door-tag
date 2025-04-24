322b6c8a3cba55ac3f1c9162106fbb6433b82b4d
Ahmad Alsakhen <73029268+Al-sakhen@users.noreply.github.com>
2025-04-24 23:00:43 +0300

    Ahmad Alsakhen 24-4-2025 11:04 PM

--- app/Http/Controllers/Dashboard/OrderController.php
+++ app/Http/Controllers/Dashboard/OrderController.php
@@ -4,14 +4,25 @@ namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Order;
 
 class OrderController extends Controller
 {
-            /**
+    /**
      * Display a listing of the resource.
      */
     public function index(Request $request)
     {
         return view('admin.orders.index');
+    }
+    public function print($orderId)
+    {
+        $order = Order::with('dealer', 'orderItems')->findOrFail($orderId);
+        $address = $order->dealer->address ? explode(',', $order->dealer->address)[0] : '---';
+        $city = $order->dealer->city ?? '---';
+        $state = $order->dealer->state ?? '---';
+        $zip_code = $order->dealer->zip_code ?? '---';
+        $phone = $order->dealer->phone ?? '---';
+        return view('prints.printOrder', compact('order', 'address', 'city', 'state', 'zip_code', 'phone'));
     }
 }
